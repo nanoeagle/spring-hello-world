6816206431ac462aa3b38b8001303203c7ba498d
nanoeagle <fistrias.ikl@gmail.com>
2021-11-12 11:00:28 +0700

    Add the package afterreturning to the package springaoparchitecture.

--- src/main/java/com/example/helloworld/springaoparchitecture/beforeadvice/BeforeAdviceDemo.java
+++ src/main/java/com/example/helloworld/springaoparchitecture/beforeadvice/BeforeAdviceDemo.java
@@ -4,11 +4,9 @@ import org.springframework.aop.framework.ProxyFactory;
 
 public class BeforeAdviceDemo {
     public static void main(String[] args) {
-        Guitarist guitarist = new Guitarist();
-
         ProxyFactory proxyFactory = new ProxyFactory();
         proxyFactory.addAdvice(new SimpleBeforeAdvice());
-        proxyFactory.setTarget(guitarist);
+        proxyFactory.setTarget(new Guitarist());
 
         Guitarist guitaristProxy = (Guitarist) proxyFactory.getProxy();
         guitaristProxy.sing("Happy Birthday");
