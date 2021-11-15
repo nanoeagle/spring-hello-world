e76b89fa6a27dd09411a28c8da03ce5ebac82925
nanoeagle <fistrias.ikl@gmail.com>
2021-11-15 14:59:53 +0700

    Add the package dynamicpointcut to the package advisorsandpointcuts.

---
+++ src/main/java/com/example/helloworld/advisorsandpointcuts/dynamicpointcut/SampleBean.java
@@ -0,0 +1,11 @@
+package com.example.helloworld.advisorsandpointcuts.dynamicpointcut;
+
+public class SampleBean {
+    public void foo(int x) {
+        System.out.println("Invoked foo() with: " + x);
+    }
+    
+    public void bar() {
+        System.out.println("Invoked bar()");
+    }
+}
